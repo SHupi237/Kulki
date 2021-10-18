4fedfb2da115c1b7d17aa56bee94413d11dec956
Stanislaw <stashupert@gmail.com>
2021-10-18 18:04:24 +0200

    Pierwsza wersja kodu

---
+++ file.ts
@@ -0,0 +1,25 @@
+var tab=[
+    [['0'],['0'],['0'],['0']],
+    [['0'],['0'],['0'],['0']],
+    [['X'],['0'],['0'],['X']],
+    [['X'],['0'],['0'],['0']]
+];
+var randomItem;
+// console.log(tab);
+var tab2=[
+    [[],[],[],[]],
+    [[],[],[],[]],
+    [[],[],[],[]],
+    [[],[],[],[]],
+];
+var value=["0","X","0","0","0"];
+
+for(var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+        randomItem=value[Math.floor(Math.random()*value.length)]
+        var elementOfArray=tab2[i][j];
+        
+    }
+    console.log(randomItem)
+}
+// console.log(tab2[1][1])
